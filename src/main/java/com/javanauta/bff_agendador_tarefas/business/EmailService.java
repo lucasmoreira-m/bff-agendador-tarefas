1b502e5db5ed69614bab01df865cf30a63b921b3
lucas <lucas.moreira.1@hotmail.com>
2026-02-07 17:20:30 -0300

    finalização do projeto

--- src/main/java/com/javanauta/bff_agendador_tarefas/business/EmailService.java
+++ src/main/java/com/javanauta/bff_agendador_tarefas/business/EmailService.java
@@ -4,7 +4,6 @@ package com.javanauta.bff_agendador_tarefas.business;
 import com.javanauta.bff_agendador_tarefas.business.DTO.TarefasDTOResponse;
 import com.javanauta.bff_agendador_tarefas.business.enums.StatusNotificacaoEnum;
 import com.javanauta.bff_agendador_tarefas.infrastructure.Client.EmailClient;
-import com.javanauta.bff_agendador_tarefas.infrastructure.Client.TarefasClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
--- src/main/java/com/javanauta/bff_agendador_tarefas/infrastructure/exceptions/BusinessException.java
+++ src/main/java/com/javanauta/bff_agendador_tarefas/infrastructure/exceptions/BusinessException.java
@@ -3,9 +3,11 @@ package com.javanauta.bff_agendador_tarefas.infrastructure.exceptions;
 
 public class BusinessException extends RuntimeException {
 
+
     public BusinessException(String mensagem){
         super(mensagem);
     }
+
 
     public BusinessException(String mensagem, Throwable throwable){
         super(mensagem, throwable);
